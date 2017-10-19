aa1666a3b57ff2cd3457101d66d52499bd4dc21e
Henrique <henrique963@hotmail.com>
2017-10-19 00:41:39 -0200

    Primeiro commit

---
+++ XF.Atividade1/XF.Atividade1/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XF.Atividade1
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnviar_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Mensagem", "Email enviado para flavio@fiap.com", "OK");
+
+        }
+      
+
+            void btnConfig_Clicked(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new ConfigPage());
+        }
+    }
+}
